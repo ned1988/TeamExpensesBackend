6db4b5cb25b181377e238ef793dc957a1c8721c6
Denys Meloshyn <denys.meloshyn@accenture.com>
2015-12-08 10:37:37 +0200

    test

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ from EventAllResource import EventAllResource
 
 # Create Flas application
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 
 # Create Restful API
 api = Api(app)
 api.add_resource(EventAllResource, '/events/all')
 
-db.init_app(app)
+# db.init_app(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
-    db.create_all()
+    # db.create_all()
