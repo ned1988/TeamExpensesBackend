02a0e782f3853d10ed5620acf72412f6e2122577
Denys Meloshyn <denys.meloshyn@accenture.com>
2016-03-24 16:20:09 +0200

    CORS

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ app = Flask(__name__)
 app.config.SWAGGER_UI_DOC_EXPANSION = 'list'
 
 CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 
